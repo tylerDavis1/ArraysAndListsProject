82bd457895844078276061087e21e33c1a5ec6d1
tyler davis <tyler.davis432@gmail.com>
2023-11-28 21:22:28 -0500

    completed arrays and lists

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace ArraysAndLists
 {
@@ -9,18 +11,15 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int array and populate it with numbers 1-10
-            
-
-            /* Create a list of type int
+            var numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+         /* Create a list of type int
              * Name the list "evens"
              */
-
-            
-            /* Create another list of type int
+            var evens = new List<int>();
+           /* Create another list of type int
              * Name the list "odds"
              */
-            
-
+            var odds = new List<int>();
              /* Using either a foreach or for loop,
              * iterate through the array you populated with 10 numbers.
              * Inside the scope of the loop,
@@ -28,17 +27,47 @@ namespace ArraysAndLists
              * If the number is even, add it to the evens list.
              * If the number is odd, add it to the odds list.
              */
+             foreach (var num in numbers)
+            {
+                if(num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
+
 
 
 
             /* Using a foreach loop,
              * display the numbers in your "evens" list
              */
-            
+            foreach (var item in evens)
+            {
+                Console.WriteLine($"{item} is an even number");
+            }
+                     
 
             /* Using a for loop,
              * display the numbers in your "odds" list
              */
+            
+            for (int i = 0; i < odds.Count; i++)
+            {
+                Console.WriteLine($"{odds[i]} These are odds!");
+                                                                               
+            }
+
+
+
+
+
+
+
+
         }
     }
 }
